5922b5f77ca87e28680e544a498d3a9f65c6272a
Mark Leo <ratrateroo@gmail.com>
2020-02-07 19:02:14 +0800

    trying to fix the script

---
+++ script.js
@@ -0,0 +1,18 @@
+/* const toggle = () => {
+    //const header = document.getElementById("header");
+    //header.classList.toggle('active');
+    const link = document.querySelector('header ul li a');
+    link.toggleAttribute
+} */
+/* const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener("click", function(){
+    toggle.classList.add('active');
+}); */
+
+window.onload = function() {
+    const toggle = document.querySelector('.toggle');
+    toggle.addEventListener("click", function(){
+    toggle.classList.add('active');
+});
+  };
