2b76274f116045542b33319913eb2b3f010feaf7
Mark Leo <ratrateroo@gmail.com>
2020-02-08 02:25:17 +0800

    made some improvements on the script file

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 
-const load = () => {
-    const toggle = document.querySelector('.toggle');
-    toggle.addEventListener("click", () => {
-        const header = document.getElementById("header");
-        header.classList.toggle('active');        
-    });
-};
-window.onload = load();
+const toggler = (link) => {
+    const withActive = document.getElementsByClassName('active');
+        if (withActive.length > 0){
+            withActive[0].classList.toggle('active');
+        }
+        link.classList.toggle('active');
+}
+
+const navlinks = document.querySelectorAll('header ul li a');
+navlinks.forEach(link => {
+    link.addEventListener("click", () => {
+            toggler(link);
+        });
+});
+
+const toggle = document.querySelector('.toggle');
+toggle.addEventListener("click", () => {
+    const header = document.getElementById("header");
+    header.classList.toggle('active');        
+});
