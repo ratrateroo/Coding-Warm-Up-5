e2f92c3e5fa0d3ee3c54af05446b98bbd9955598
Mark Leo <ratrateroo@gmail.com>
2020-02-08 01:27:41 +0800

    fixed the function for toggling active class

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-/* const toggle = () => {
-    //const header = document.getElementById("header");
-    //header.classList.toggle('active');
-    const link = document.querySelector('header ul li a');
-    link.toggleAttribute
-} */
-/* const toggle = document.querySelector('.toggle');
 
-toggle.addEventListener("click", function(){
-    toggle.classList.add('active');
-}); */
-
-/* window.onload = function() {
-    const toggle = document.querySelector('.toggle');
-    toggle.addEventListener("click", function(){
-    toggle.classList.add('active');
-});
-  }; */
 const load = () => {
     const toggle = document.querySelector('.toggle');
     toggle.addEventListener("click", () => {
-        // if (!toggle.classList.contains('active')) {
-        //     toggle.classList.add('active');
-        // } else {
-        //     toggle.classList.remove('active');
-        // }
         const header = document.getElementById("header");
-        header.classList.toggle('active');
-        
+        header.classList.toggle('active');        
     });
 };
-
 window.onload = load();
