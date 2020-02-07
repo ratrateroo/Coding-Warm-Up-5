e2e6bf14df08b2afe7b3085a84454b753991de05
Mark Leo <ratrateroo@gmail.com>
2020-02-07 19:29:42 +0800

    fixed the script for toggle button

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ toggle.addEventListener("click", function(){
     toggle.classList.add('active');
 }); */
 
-window.onload = function() {
+/* window.onload = function() {
     const toggle = document.querySelector('.toggle');
     toggle.addEventListener("click", function(){
     toggle.classList.add('active');
 });
-  };
+  }; */
+const load = () => {
+    const toggle = document.querySelector('.toggle');
+    toggle.addEventListener("click", () => {
+        // if (!toggle.classList.contains('active')) {
+        //     toggle.classList.add('active');
+        // } else {
+        //     toggle.classList.remove('active');
+        // }
+        const header = document.getElementById("header");
+        header.classList.toggle('active');
+        
+    });
+};
+
+window.onload = load();
